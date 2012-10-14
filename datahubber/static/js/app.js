a78c92ccc39d9efddbb1a96f482670de88429981
Rufus Pollock <rufus.pollock@okfn.org>
2012-10-14 03:28:07 +0100

    [#7,addendum][s]: tidy up app.js in line with last commit (was getting js errors).

--- datahubber/static/js/app.js
+++ datahubber/static/js/app.js
@@ -1,20 +1,6 @@
 jQuery(document).ready(function($) {
-  var catalog = new Catalog.Models.Catalog();
   window.catalog = new Catalog.Views.Application({
-    model: catalog,
     el: $('body')
-  });
-  Backbone.history.start();
-
-  // retrieve datasets ...
-  var datasetNames = ['gdp', 'us-cpi'];
-  var retrieved = 0;
-  _.each(datasetNames, function(name) {
-    catalog.getByName(name, function(err, ds) {
-      retrieved += 1;
-      if (retrieved == datasetNames.length) {
-      }
-    });
   });
 });
 
