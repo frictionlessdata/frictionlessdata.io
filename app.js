063d8fc2b78bea65a0ecdcda1974aa8f29103cec
Rufus Pollock <rufus.pollock@okfn.org>
2013-02-15 10:05:38 +0000

    [#14,refactor][m]: home page working but no datasets yet (continuing refactor to js).

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express')
   , path = require('path')
+  , fs = require('fs')
+  , nunjucks = require('nunjucks')
   ;
 
 var app = express();
@@ -7,7 +9,8 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env.PORT || 5000);
   app.set('views', __dirname + '/templates');
-  app.set('view engine', 'jade');
+  // app.set('view engine', 'html');
+  // app.engine('html', require('hbs').__express);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -15,9 +18,14 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('templates'));
+env.express(app);
+
+app.get('/', function(req, res) {
+  res.render('home.html', {
+    datasets: []
+  });
+})
 
 app.listen(app.get('port'), function() {
   console.log("Listening on " + app.get('port'));
