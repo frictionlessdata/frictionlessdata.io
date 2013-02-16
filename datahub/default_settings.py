62eb958033d414ff8c3a6329fa8a1697c4762694
Rufus Pollock <rufus.pollock@okfn.org>
2013-02-16 09:47:45 +0000

    [#14,delete][s]: remove all remaining python code - fixes #14.

--- datahub/default_settings.py
+++
@@ -1,8 +0,0 @@
-DEBUG = True
-
-HOST = '0.0.0.0'
-PORT = 5000
-
-SECRET_KEY = 'my-session-secret'
-
-DATA_PACKAGES_INDEX = 'http://raw.github.com/datasets/registry/master/index.json'
--- heroku_settings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-DEBUG = False
-SECRET_KEY = os.environ.get('SECRET_KEY')
-PORT = os.environ.get('PORT', 5000)
-
