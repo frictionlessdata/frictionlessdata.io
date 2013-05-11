f82c04a324fb89df302748c73363d0e08791493e
Rufus Pollock <rufus.pollock@okfn.org>
2013-05-11 18:06:15 +0100

    [#45,tests][m]: some initial mocha-based tests of the site using supertest.

---
+++ tests/site.js
@@ -0,0 +1,31 @@
+var request = require('supertest')
+  , express = require('express')
+  , assert = require('assert')
+  ;
+
+var app = require('../app.js').app;
+
+describe('GET /tools/creator', function(){
+  it('respond with json', function(done){
+    request(app)
+      .get('/tools/creator.json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  })
+});
+
+describe('GET /tools/creator', function(){
+  it('responds with correct json', function(done){
+    var gpurl = 'https://raw.github.com/datasets/gold-prices/master/data/data.csv';
+    var url = '/tools/creator.json?resource.url=' + gpurl;
+    request(app)
+      .get(url)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) done(err);
+        var out = res.body;
+        assert.equal(out.resources[0].url, gpurl);
+        done();
+      });
+  })
+})
