d04d58d488e73299a8be55fef783357f0e4b964d
Rufus Pollock <rufus.pollock@okfn.org>
2013-06-23 21:45:55 +0100

    [#42,tools][m]: tool function to load and normalize datapackage.json files.

--- tests/tools.js
+++ tests/tools.js
@@ -23,3 +23,65 @@ describe('validate', function() {
   });
 });
 
+
+var dpin = {
+  "name": "gold-prices",
+  "readme": "Abc\n\nXyz",
+  "resources": [
+    {
+      "path": "data/data.csv",
+      "format": "csv",
+      "schema": {
+        "fields": [
+          {
+            "type": "date",
+            "id": "date"
+          },
+          {
+            "type": "number",
+            "id": "price"
+          }
+        ]
+      }
+    }
+  ]
+};
+var sourceUrl = 'https://raw.github.com/datasets/gold-prices/master/datapackage.json'; 
+var sourceUrlBase = 'https://raw.github.com/datasets/gold-prices/master/'; 
+
+describe('normalize', function() {
+  it('works in basic case', function() {
+    dpout = tools.normalize(dpin, sourceUrl);
+    assert.equal(dpout.resources[0].url, sourceUrlBase + 'data/data.csv');
+    assert.equal(dpout.description, 'Abc');
+  });
+});
+
+describe('load', function() {
+  it('works in basic case', function(done) {
+    tools.load(sourceUrl, function(err, dpout) {
+      assert(err === null);
+      assert.equal(dpout.resources[0].url, sourceUrlBase + 'data/data.csv');
+      assert(dpout.readme.length > 50);
+      assert(dpout.description.length <  150);
+      done();
+    });
+  });
+
+  it('works with 404', function(done) {
+    var badUrl = 'https://raw.github.com/datasets/gold-prices/master/xyz.txt';
+    tools.load(badUrl, function(err, dpout) {
+      assert(err!=null);
+      done();
+    });
+  });
+
+  it('works with bad data', function(done) {
+    var csvurl = 'https://raw.github.com/datasets/gold-prices/master/README.md'; 
+    dpout = tools.load(csvurl, function(err, dpout) {
+      assert.equal(err.message.indexOf('datapackage.json is invalid JSON'), 0);
+      done();
+    });
+  });
+});
+
--- tools.js
+++ tools.js
@@ -1,4 +1,8 @@
-var JSV = require("JSV").JSV;
+var JSV = require("JSV").JSV
+  , request = require('request')  
+  , marked = require('marked')
+  ;
+
 
 var dpSchema = {
   "title": "Data Package",
@@ -42,5 +46,65 @@ exports.dpValidate= function(raw) {
   ];
   var report = env.validate(json, dpSchema);
   return report.errors;
+}
+
+// Load and normalize a datapackage.json
+// Normalize means converting relative paths to absolute paths
+exports.load = function(datapackage_url, cb) {
+  var base = datapackage_url.replace(/datapackage.json$/g, '');
+  request(datapackage_url, function(error, response, body) {
+    if (error) {
+      cb(error);
+      return;
+    } else if (response.statusCode != 200) {
+      cb({message: 'Unable to access file. Status code: ' + response.statusCode});
+      return;
+    }
+    // TODO: handle bad JSON
+    try {
+      var datapackage = JSON.parse(body);
+    } catch(e) {
+      cb({message: 'datapackage.json is invalid JSON. Details: ' + e.message});
+      return;
+    }
+    var readme_url = base + 'README.md'
+    request(readme_url, function(err, resp, body) {
+      if (!err) {
+        datapackage['readme'] = body.replace(/\r\n/g, '\n');
+      }
+      datapackage = exports.normalize(datapackage, base);
+      cb(null, datapackage); 
+    });
+  });
+};
+
+// url = url to datapackage.json or the root directory in which it is contained
+exports.normalize = function(datapackage, url) {
+  var base = url.replace(/datapackage.json$/g, '');
+  // ensure certain fields exist
+  if (!'description' in datapackage) {
+    datapackage.description  = '';
+  }
+  // set description as first paragraph of readme if we no description
+  if (!datapackage.description && 'readme' in datapackage) {
+    // first extract plain text ...
+    // TODO: reinstate this
+    // html = markdown(unicode(datapackage['readme'], 'utf8'))
+    // plain = strip_tags(html).split('\n\n')[0].replace(' \n', '').replace('\n', ' ')
+    // datapackage['description'] = plain.encode('utf8')
+    datapackage.description = datapackage.readme.split('\n\n')[0]
+  } else if (!datapackage.readme) {
+    datapackage.readme = datapackage.description;
+  }
+
+  datapackage.readme_html = marked(datapackage.readme);
+
+  datapackage.resources.forEach(function(info) {
+    if (!info.url && info.path) {
+      info.url = base + info.path;
+    }
+  });
+
+  return datapackage;
 }
 
