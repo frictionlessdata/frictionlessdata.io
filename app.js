88301b67f522e1371ca36cb4c189498e77cf7e29
thechunsik <thechunsik@gmail.com>
2015-09-25 01:34:02 +0900

    updated some old libraries - upgraded deprecated libraries - bodyParser and method-override - upgraded to Font Awesome 4 which is issue #167

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express')
   , path = require('path')
   , nunjucks = require('nunjucks')
+  , bodyParser = require('body-parser')
+  , methodOverride = require('method-override')
   , routes = require('./routes')
   ;
 
@@ -14,18 +16,17 @@ var CORSSupport = function(req, res, next) {
   next();
 }
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 5000);
-  app.set('views', __dirname + '/views');
-  // app.set('view engine', 'html');
-  // app.engine('html', require('hbs').__express);
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(CORSSupport);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+app.set('port', process.env.PORT || 5000);
+app.set('views', __dirname + '/views');
+// app.set('view engine', 'html');
+// app.engine('html', require('hbs').__express);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(CORSSupport);
+app.use(methodOverride());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')));
 
 var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('views'));
 env.express(app);
