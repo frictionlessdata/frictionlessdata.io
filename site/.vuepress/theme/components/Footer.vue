30abd30036572e2efa5630c54af8654cd3a25684
Patricio Del Boca <patriciodelboca@gmail.com>
2025-08-14 13:15:50 +0000

    Copy okfn.org style

--- site/.vuepress/theme/components/Footer.vue
+++ site/.vuepress/theme/components/Footer.vue
@@ -4,17 +4,17 @@
   <div class="flex flex-row flex-wrap gap-4">
     <div class="flex-none">
       <a target="_blank" href="https://okfn.org">
-        <img src="{% static '/images/lg-okfn-black.svg' %}" class="h-20" alt="Open Knowledge Foundation">
+        <img src="/img/footer/lg-okfn-black.svg" class="h-20" alt="Open Knowledge Foundation">
       </a>
     </div>
       <ul class="flex flex-wor flex-grow gap-10 justify-end items-center">
-      <li><a href="https://us9.list-manage.com/subscribe?u=73f922f8e68e21a10efa21f02&id=32d370a7fc" target="_blank"><img src="{% static '/images/footer/mailinglist.svg' %}"></a></li>
-      <li><a href="https://github.com/okfn" target="_blank"><img src="{% static '/images/footer/github.svg' %}"></a></li>
-      <li><a href="https://www.linkedin.com/company/open-knowledge-foundation/" target="_blank"><img src="{% static '/images/footer/linkedin.svg' %}"></a></li>
-      <li><a href="https://fosstodon.org/@okfn" target="_blank"><img src="{% static '/images/footer/mastodon.svg' %}"></a></li>
-      <li><a href="https://twitter.com/okfn" target="_blank"><img src="{% static '/images/footer/x.svg' %}"></a></li>
-      <li><a href="https://www.youtube.com/@OpenKnowledgeFoundation/videos" target="_blank"><img src="{% static '/images/footer/youtube.svg' %}"></a></li>
-      <li><a href="https://flickr.com/photos/okfn/" target="_blank"><img src="{% static '/images/footer/flickr.svg' %}"></a></li>
+      <li><a href="https://us9.list-manage.com/subscribe?u=73f922f8e68e21a10efa21f02&id=32d370a7fc" target="_blank"><img src="/img/footer/mailinglist.svg"></a></li>
+      <li><a href="https://github.com/okfn" target="_blank"><img src="/img/footer/github.svg"></a></li>
+      <li><a href="https://www.linkedin.com/company/open-knowledge-foundation/" target="_blank"><img src="/img/footer/linkedin.svg"></a></li>
+      <li><a href="https://fosstodon.org/@okfn" target="_blank"><img src="/img/footer/mastodon.svg"></a></li>
+      <li><a href="https://twitter.com/okfn" target="_blank"><img src="/img/footer/x.svg"></a></li>
+      <li><a href="https://www.youtube.com/@OpenKnowledgeFoundation/videos" target="_blank"><img src="/img/footer/youtube.svg"></a></li>
+      <li><a href="https://flickr.com/photos/okfn/" target="_blank"><img src="/img/footer/flickr.svg"></a></li>
       </ul>
   </div>
   <div class="flex flex-row flex-wrap justify-between gap-10 py-4">
@@ -41,23 +41,23 @@
   </div>
   <div>
   <div class="flex flex-row items-center gap-4 mb-4">
-      <a href="https://en.wikipedia.org/wiki/MIT_License"><img src="{% static '/images/footer/subfooter/license.png' %}"></a>
+      <a href="https://en.wikipedia.org/wiki/MIT_License"><img src="/img/footer/subfooter/license.png"></a>
       <div>Source code available under the MIT license.</div>
   </div>
   <div class="flex flex-row items-center gap-4 mb-4"> 
-    <a href="https://creativecommons.org/licenses/by/4.0/"><img src="{% static '/images/footer/subfooter/creative-commons.png' %}"></a>
+    <a href="https://creativecommons.org/licenses/by/4.0/"><img src="/img/footer/subfooter/creative-commons.png"></a>
     <div>Content on this site, made by Open Knowledge, is licensed under a Creative Commons Attribution 4.0 International License.</div>
   </div>
   <div class="flex flex-row items-center gap-4 mb-4">
-    <a href="https://okfn.org/en/who-we-are/governance/"><img src="{% static '/images/footer/subfooter/ngo-source.png' %}"></a>
+    <a href="https://okfn.org/en/who-we-are/governance/"><img src="/img/footer/subfooter/ngo-source.png"></a>
     <div>Open Knowledge is certified by NGOsource in accordance with the Equivalency Determination (ED).</div>
   </div>
   <div class="flex flex-row items-center gap-4 mb-4">
-    <a href="https://www.ckan.org"><img width="82px" src="{% static '/images/footer/subfooter/ckan.svg' %}"></a>
+    <a href="https://www.ckan.org"><img width="82px" src="/img/footer/subfooter/ckan.svg"></a>
     <div>CKAN assets are held in trust by Open Knowledge, with best practice policies on governing openly and for use of the trademark.</div>
   </div>
   <div class="flex flex-row items-center gap-4 mb-4">
-    <a href="https://www.digitalpublicgoods.net/"><img src="{% static '/images/footer/subfooter/dpga.png' %}"></a>
+    <a href="https://www.digitalpublicgoods.net/"><img src="/img/footer/subfooter/dpga.png"></a>
     <div>Open Knowledge is part of the Digital Public Goods Alliance, accelerating the attainment of the Sustainable Development Goals.</div>
   </div>
   </div>
@@ -74,39 +74,27 @@
     padding-bottom: 3rem;
 }
 
+.main-footer .master {
+    position: relative;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 84rem;
+}
+
+a {
+    font-weight: 700;
+    transition-property: color,background-color,border-color,text-decoration-color,fill,stroke;
+    transition-duration: .2s;
+    transition-timing-function: cubic-bezier(.4,0,.2,1);
+    color: inherit;
+    text-decoration: inherit;
+}
+a:hover {
+  color: white;
+}
+
 li {
   list-style: none;
 }
 
-.footer-bg {
-  background-color: #00d1ff;
-  border-top: 1px solid #cfe1e6;
-}
-
-li > a {
-  color:black;
-  font-weight: 700;
-  font-size: 1rem;
-}
-
-li > a:hover {
-  color: white;
-}
-
-.footer-title {
-  color: black;
-  font-size: 1.1rem;
-  font-weight: 700;
-}
-.footer-items{
-  margin-left: 8em;
-}
-
-.footer-logo{
-  margin-left: 2.5em;
-}
-
-.bottom-padding{
-  padding-bottom: 1em;
-}
 </style>
